345e1f431ee254d92ed885ac906c19024c91cc65
vineesh777 <vineeshr777@gmail.com>
2023-02-21 09:34:45 +0530

    unwanted files deleted

--- backend/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const fs = require("firebase-admin");
-const serviceAccount = require("./service-account.json");
-fs.initializeApp({
-  credential: fs.credential.cert(serviceAccount),
-});
-const db = fs.firestore();
-
-module.exports = db;
